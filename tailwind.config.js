f9db97e00190f9686e315fd14295899604488a18
BenGregory23 <bengregorygbr@gmail.com>
2023-10-25 12:54:57 +0200

    trying to fix tailwind in prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ import { nextui } from "@nextui-org/react";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    'index.html',
+    './index.html',
     './src/**/*.{html,ts,tsx}',
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
